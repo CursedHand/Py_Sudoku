954d0011c542794d55c8486e7fee1c6573211d72
CursedHand <87908317+CursedHand@users.noreply.github.com>
2022-02-21 15:24:07 -0800

    Fixed highlight assist flickering

--- GenerateGrid.py
+++ GenerateGrid.py
@@ -1,90 +1,85 @@
+from operator import ge
 from fltk import *
 
-def create_grid(width, height):
+def create_grid(width, height, margin):
+
     grid = []
-    box_size = int(width/9)
     for col in range(9):
         row_list = []
         for row in range(9):
-            if col == 2 or col == 5:
-                if row != 2 and row != 5 and row != 3 and row != 6:
-                    row_list.append(Fl_Input((box_size*col), (box_size*row)+(height-width), box_size-2, box_size))
-                elif row == 2 or row == 5:
-                    row_list.append(Fl_Input((box_size*col), (box_size*row)+(height-width), box_size-2, box_size-2))
-                else:
-                    row_list.append(Fl_Input((box_size*col), (box_size*row)+(height-width)+2, box_size-2, box_size-2))
-            elif col == 3 or col == 6:
-                if row != 3 and row != 6 and row != 2 and row != 5:
-                    row_list.append(Fl_Input((box_size*col)+2, (box_size*row)+(height-width), box_size-2, box_size))
-                elif row == 3 or row == 6:
-                    row_list.append(Fl_Input((box_size*col)+2, ((box_size*row)+(height-width))+2, box_size-2, box_size-2))
-                else:
-                    row_list.append(Fl_Input((box_size*col)+2, ((box_size*row)+(height-width)), box_size-2, box_size-2))
-            elif row == 2 or row == 5:
-                row_list.append(Fl_Input((box_size*col), (box_size*row)+(height-width), box_size, box_size-2))
-            elif row == 3 or row == 6:
-                row_list.append(Fl_Input((box_size*col), (box_size*row)+(height-width)+2, box_size, box_size-2))
-            else:
-                row_list.append(Fl_Input((box_size*col), (box_size*row)+(height-width), box_size, box_size))
+
+            x, y, w, h = get_size(row, col, width, height, margin)
+            row_list.append(Fl_Input(x, y, w, h))
+            
             row_list[-1].maximum_size(3)
             row_list[-1].textsize(43)
             row_list[-1].box(FL_THIN_UP_BOX)
         grid.append(row_list)
     return grid
 
+def get_size(row, col, width, height, margin):
+    box_size = int(width/9)
+    x_pos = box_size*col
+    y_pos = box_size*row + height - width
+    if row == 2 or row == 5:
+        if col == 2 or col == 5:
+            return(x_pos, y_pos, box_size-margin, box_size-margin)
+        elif col == 3 or col == 6:
+            return(x_pos+margin, y_pos, box_size-margin, box_size-margin)
+        else:
+            return(x_pos, y_pos, box_size, box_size-margin)
+    elif row == 3 or row == 6:
+        if col == 2 or col == 5:
+            return(x_pos, y_pos+margin, box_size-margin, box_size-margin)
+        elif col == 3 or col == 6:
+            return(x_pos+margin, y_pos+margin, box_size-margin, box_size-margin)
+        else:
+            return(x_pos, y_pos+margin, box_size, box_size-margin)
+    elif col == 2 or col == 5:
+        return(x_pos, y_pos, box_size-margin, box_size)
+    elif col == 3 or col == 6:
+        return(x_pos+margin, y_pos, box_size-margin, box_size)
+    else:
+        return(x_pos, y_pos, box_size, box_size)
+
+
+def resize_cb(Sudoku, size, grid, menu, width, height, margin):
+    if size == 0: #Small
+        Sudoku.size(450, 500)
+        width, height = 450, 500
+        for row in grid:
+            for box in row:
+                box.textsize(25)
+    elif size == 1: #Medium
+        Sudoku.size(711, 761)
+        width, height = 711, 761
+        for row in grid:
+            for box in row:
+                box.textsize(43)
+    else:   #Large
+        Sudoku.size(918, 968)
+        width, height = 918, 968
+        for row in grid:
+            for box in row:
+                box.textsize(55)
+    menu.size(width, height-width)
+    Sudoku.redraw()
+
+    c = 0
+    for row in grid:
+        r = 0
+        for but in row:
+            x, y, w, h = get_size(r, c, width, height, margin)
+            but.resize(x,y,w,h)
+            r += 1
+        c += 1
+    Sudoku.redraw()
+    return width, height
+
+
+
 
 def fill_grid(grid):
-
-    grid[0][0].value("8")
-    grid[3][0].value("1")
-    grid[5][0].value("7")
-    grid[6][0].value("4")
-    grid[7][0].value("6")
-    grid[8][0].value("9")
-
-    grid[2][1].value("5")
-    grid[3][1].value("3")
-    grid[5][1].value("6")
-    grid[7][1].value("8")
-
-    grid[5][2].value("4")
-    grid[7][2].value("3")
-    grid[8][2].value("5")
-
-    grid[0][3].value("9")
-    grid[1][3].value("8")
-    grid[4][3].value("7")
-    grid[7][3].value("1")
-
-    grid[0][4].value("2")
-    grid[1][4].value("5")
-    grid[2][4].value("7")
-    grid[3][4].value("8")
-    grid[4][4].value("3")
-    grid[7][4].value("9")
-
-    grid[1][5].value("1")
-    grid[2][5].value("3")
-    grid[6][5].value("8")
-    grid[7][5].value("7")
-    grid[8][5].value("2")
-
-    grid[2][6].value("8")
-    grid[6][6].value("9")
-
-    grid[0][7].value("4")
-    grid[1][7].value("2")
-    grid[3][7].value("7")
-    grid[5][7].value("3")
-    grid[6][7].value("1")
-
-    grid[1][8].value("6")
-    grid[2][8].value("1")
-    grid[3][8].value("9")
-    grid[6][8].value("3")
-
-
-
     squares = [[], [], [], [], [], [], [], [], []]
     rows = [[], [], [], [], [], [], [], [], []]
     colums = [[], [], [], [], [], [], [], [], []]
